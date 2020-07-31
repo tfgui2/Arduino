080dbe32c3713728bbf1c6bd3fac0a5375ba2bbc
tfgui <tfgui2@gmail.com>
2020-07-31 18:40:17 +0900

    a

--- fsxconnect/fsxconnect.ino
+++ fsxconnect/fsxconnect.ino
@@ -1,9 +1,11 @@
+
+#include "ClientEvents.h"
+
 // oled 
 // pin: scl(A5) sda(A4)
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
-
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -20,10 +22,34 @@ Button bt1(8, button1Click);
 // encoder
 #include <RotaryEncoder.h>
 void EncRotate(int dir);
+void EncRotate2(int dir);
 void EncPush();
-RotaryEncoder enc(2, 3, 4, EncRotate, EncPush);
+RotaryEncoder enc(2,3,4, EncRotate, EncPush);
+RotaryEncoder enc2(5,6,7, EncRotate2, EncPush);
 
+enum MODE
+{
+  MODE_AP,
+  MODE_AUDIO,
+  MODE_COM1,
+  MODE_COM2,
+  MODE_NAV1,
+  MODE_NAV2,
+  MODE_MAX
+};
+int modeIndex = 0;
 
+// encoder table
+int8_t enctable[][4] = 
+{
+  {-1, -1, -1, -1}, // ap
+  {-1, -1, -1, -1}, // ad
+  {COM_RADIO_WHOLE_DEC, COM_RADIO_WHOLE_INC, COM_RADIO_FRACT_DEC, COM_RADIO_FRACT_INC}, // com1
+  {COM2_RADIO_WHOLE_DEC, COM2_RADIO_WHOLE_INC, COM2_RADIO_FRACT_DEC, COM2_RADIO_FRACT_INC}, // 
+  {NAV1_RADIO_WHOLE_DEC, NAV1_RADIO_WHOLE_INC, NAV1_RADIO_FRACT_DEC, NAV1_RADIO_FRACT_INC}, // 
+  {NAV2_RADIO_WHOLE_DEC, NAV2_RADIO_WHOLE_INC, NAV2_RADIO_FRACT_DEC, NAV2_RADIO_FRACT_INC}, // 
+  
+};
 
 // internal val
 int led = 13;
@@ -38,21 +64,9 @@ char text[][10] =
   "Nav 1",
   "Nav 2",
 };
-enum MODE
-{
-  MODE_AP,
-  MODE_AUDIO,
-  MODE_COM1,
-  MODE_COM2,
-  MODE_NAV1,
-  MODE_NAV2,
-  MODE_MAX
-};
-int modeIndex = 0;
+
 int showMenu = 0; // 0 menu, 1 sub
 char encval2[10] = "";
-
-#include "ClientEvents.h"
 
 
 void setup() {
@@ -69,6 +83,7 @@ void setup() {
 void loop() {
   bt1.run();
   enc.run();
+  enc2.run();
 
   if (isUpdate)
   {
@@ -148,45 +163,42 @@ void EncRotate(int dir)
     return;
   }
   
-  int key = -1;
+  int8_t key = -1;
   bool alt = dir > 0;
-
-  switch (modeIndex) {
-    case MODE_AP:
-
-      break;
-
-    case MODE_AUDIO:
-      break;
-
-    case MODE_COM1:
-      key = alt ? COM_RADIO_WHOLE_INC : COM_RADIO_WHOLE_DEC;
-      break;
-
-    case MODE_COM2:
-      key = alt ? COM2_RADIO_WHOLE_INC : COM2_RADIO_WHOLE_DEC;
-      break;
-
-    case MODE_NAV1:
-      key = alt ? NAV1_RADIO_WHOLE_INC : NAV1_RADIO_WHOLE_DEC;
-      break;
-
-    case MODE_NAV2:
-      key = alt ? NAV2_RADIO_WHOLE_INC : NAV2_RADIO_WHOLE_DEC;
-      break;
-  }
-
-  if (key > -1) {
+  key = enctable[modeIndex][alt];
+  
+  if (key > (-1)) {
     Serial.write(key);
   }
 
-  sprintf(encval2, "value:%d", dir);
+  sprintf(encval2, "value:%d", key);
   isUpdate = true;
 }
 
+
+void EncRotate2(int dir)
+{
+  if (showMenu) {
+    MenuRotate(dir);
+    return;
+  }
+  
+  int8_t key = -1;
+  bool alt = dir > 0;
+  key = enctable[modeIndex][alt + 2];
+  
+  if (key > (-1)) {
+    Serial.write(key);
+  }
+
+  sprintf(encval2, "value:%d", key);
+  isUpdate = true;
+}
+
+
 void EncPush()
 {
-  int key = -1;
+  int8_t key = -1;
 
   switch (modeIndex) {
     case MODE_AP:
